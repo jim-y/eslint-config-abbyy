da09e585d48b16c182caa862f4dcc9d313967833
Attila Kling <attila.kling@gmail.com>
2022-03-19 23:29:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+ * @rushstack/eslint-patch is used to include plugins as dev
+ * dependencies instead of imposing them as peer dependencies
+ *
+ * https://www.npmjs.com/package/@rushstack/eslint-patch
+ */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  extends: ["plugin:abbyy/recommended"],
+  plugins: ["abbyy"],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  }
+}
